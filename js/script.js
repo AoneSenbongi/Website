4fa1418805643efb5bff07c06d28df9dc2514ab7
千本木碧音 <aonesenbongi@gmail.com>
2026-08-07 03:13:59 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,13 +45,3 @@ window.addEventListener("scroll", () => {
     }
 
 });
-
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".hamburger-menu");
-
-
-hamburger.addEventListener("click",()=>{
-
-    menu.classList.toggle("active");
-
-});
